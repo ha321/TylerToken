6cff2f287aef460bfb0bd9b41bc1a397ea38c4b9
Subraiz Ahmed <subraizahmed@Subraizs-MBP-2.home>
2018-07-13 20:29:23 -0400

    Token Creation

--- token/truffle.js
+++ token/truffle.js
@@ -1,18 +1,39 @@
-/*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
- * function when declaring them. Failure to do so will cause commands to hang. ex:
- * ```
- * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
- *     },
- *     network_id: '1',
- *     gas: 4500000,
- *     gasPrice: 10000000000,
- *   },
- */
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var testnetMnemonic =
+  "mutual life endless direct direct shell rich dance scheme hunt into lend hero spare coral";
+var testnetWalletAddress = "0xbf20394C1a0F6784e5A1fD6c4C2e6baCDb8DD4c6";
+var testnetContractAddress = "0xeb930c643384ec45fc80268e2019f40e33fe17f4";
+
+var mainnetMnemonic = "XXX";
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(
+          testnetMnemonic,
+          "https://ropsten.infura.io/F5xdDA6sjrIgRJNUivmc"
+        );
+      },
+      network_id: 3,
+      gas: 4800000,
+      gasPrice: 24000000000
+    },
+    mainnet: {
+      provider: function() {
+        return new HDWalletProvider(
+          mainnetMnemonic,
+          "https://mainnet.infura.io/F5xdDA6sjrIgRJNUivmc"
+        );
+      },
+      network_id: 1,
+      gas: 4800000,
+      gasPrice: 24000000000
+    }
+  }
 };
